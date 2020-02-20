6e3ada591603f3a410e959aff0443f0175993940
Reinoso <jreinoso@amazon.com>
2020-02-20 08:41:25 -0500

    project flow is working way better, i only need to find a way to run the 400 error when no post is found for that user

--- post/posts-router.js
+++ post/posts-router.js
@@ -55,22 +55,23 @@ router.get("/:id", (req, res)=> {
 // with the specified id.
 
 router.get("/:id/comments", (req, res) => {
+    
     post.findCommentById(req.params.id)
-
     .then((comments)=> {
-      if ( comments) {
-        res.status(200).json(comments)
-      }else{
-          res.status(404).json({
-              message: "The post with the specified ID does not exist."
-          })
-      }
-
+        // if comments comes with an empty body, then it run that error message
+     if(comments == ""){
+      res.status(404).json({
+          message:"The post with the specified ID does not exist."
+      })
+      //otherwise if comments comes with info in the body, it will return this info as a json
+     }else{
+         res.status(200).json(comments)
+     }
         
     })
 
     .catch((error) => {
-        res.status(404).json({
+        res.status(500).json({
             message:"The comments information could not be retrieved." 
         })
     })
@@ -158,23 +159,36 @@ router.delete("/:id", (req,res) => {
 //the post with the specified id using information sent inside of the request body.
 
 router.post("/:id/comments", (req, res) => {
-      if(!req.body.text) {
+    
+    //Here im making variables since data it will have to be saved first before inserting them to the data base
+    
+    const {id} =  req.params.id;
+    //im creating a copy of the body, and creating a post_id key:value pair with the new id of the comment being created.
+    const data = {...req.body, post_id: req.params.id}
+    
+    // here im pretty much saying that if the client (front end) does not provide to us an input with text: "comments needed to be created"
+    // to return an error
+    if(!req.body.text) {
          return  res.status(400).json({
               message: "Please provide text for the comment."
           })
       }
-      post.findById(req.params.id)
-      
-       .then(comment => {
-           if (comment) {
-            post.insertComment({...req.body, post_id: req.params.id})
-            post.findCommentById(req.body.post_id)
-            res.status(201).json(comment)
+ //here im finding first the post related with the inputed id
+      post.findById(id)
+             .then(comment => {
+        //this logic has to be fixed so that if the id does not return any body it would run this error
+           if (!comment) {
+            res.status(404).json({
+            message: "The post with the specified ID does not exist."
+            })
            }
+
            else {
-               res.status(404).json({
-                   message: "The post with the specified ID does not exist."
-               })
+            //passing data variable thata contains the copy of the body and post_id
+           // to insertComment function thats in the DB.JS file(database)
+            post.insertComment(data)
+            //here im just returning new text created along a 201 created status.
+            res.status(201).json(req.body)
            }
        })
 
